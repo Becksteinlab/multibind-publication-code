348c85b465e60b9add9dd54a841b6549d98fdbb7
Ian Kenney <ikenney@asu.edu>
2022-05-02 14:34:22 -0700

    Quick sync

--- thermodynamic_rates/lua/proton_on_off_rates.lua
+++ thermodynamic_rates/lua/proton_on_off_rates.lua
@@ -3,9 +3,9 @@
 local chem = require("calc")
 
 print("--- Regression ---")
-chem.rates(7, 9, 5, "hemi") -- control, should be 17.588 for the koff
+chem.rates(7, 9, 5, "hemi", true) -- control, should be 17.588 for the koff
 print(string.rep("-", 36))
 
 print("--- Disk ---")
-chem.rates(7, 9, 5, "disk")
+chem.rates(7, 9, 5, "disk", true)
 print(string.rep("-", 36))
