7622a6afd5de4c92d72be7ae589f6dc33cbdb38d
Ian Kenney <ikenney@asu.edu>
2022-05-03 09:06:26 -0700

    full antiporter code

---
+++ full_antiporter_example/equil.py
@@ -0,0 +1,176 @@
+#!/usr/bin/env python
+
+import matplotlib.pyplot as plt
+import numpy as np
+from pathlib import Path
+import seaborn as sns
+
+state_colors = {
+        'IF0': '#e41a1c', #  RED
+        'IFH': '#377eb8', #  BLUE
+        'IFNA': '#4daf4a', #  GREEN
+        'OF0': '#984ea3', #  PURPLE
+        'OFH': '#ff7f00', #  ORANGE
+        'OFNA': '#ffff33', #  YELLOW
+        }
+
+state_map = {
+    'IF0': 'IF(0)',
+    'IFH': r'IF(H$^+$)',
+    'IFNA': r'IF(Na$^+$)',
+    'OF0': 'OF(0)',
+    'OFH': r'OF(H$^+$)',
+    'OFNA': r'OF(Na$^+$)',
+}
+
+sod_colors = [
+    '#d0d1e6',
+    '#a6bddb',
+    '#74a9cf',
+    '#3690c0',
+    '#0570b0',
+    '#045a8d',
+    '#023858',
+]
+
+
+def run(scanner):
+    img_dir = Path('.') / 'img' / 'equil'
+    conf_dg_dir = img_dir / 'conf_dg'
+    free_energy_dir = img_dir / 'free_energy'
+    microstate_probs_dir = img_dir / 'msp'
+    protonation_dg_dir = img_dir / 'prot_dg'
+    sod_bind_dg_dir = img_dir / 'sod_bind_dg'
+
+    conf_dg_dir.mkdir(parents=True, exist_ok=True)
+    free_energy_dir.mkdir(parents=True, exist_ok=True)
+    microstate_probs_dir.mkdir(parents=True, exist_ok=True)
+    protonation_dg_dir.mkdir(parents=True, exist_ok=True)
+    sod_bind_dg_dir.mkdir(parents=True, exist_ok=True)
+
+    concentrations = dict()
+    concentrations['Na+'] = [0.01, 0.1, 0.15, 0.2, 0.25]
+    concentrations['H+'] = np.linspace(0, 15, 15 * 4)
+    concentrations['H+'] = np.concatenate((concentrations['H+'], [8.0]))
+    concentrations['H+'].sort()
+
+    scanner.run(concentrations, svd=True)
+
+    pH = concentrations['H+']
+    Na = concentrations['Na+']
+
+    conf_dg = np.zeros((len(Na), len(pH)))
+    prot_dg = np.zeros_like(conf_dg)
+    sod_dg = np.zeros_like(conf_dg)
+
+    for i, c_hp in enumerate(pH):
+        for j, c_na in enumerate(Na):
+            out_c = {'pH': c_hp, 'Na+': c_na}
+            conf_dg[j, i], _ = scanner.effective_energy_difference('conf', 'inward', 'outward', **out_c)
+            prot_dg[j, i], _ = scanner.effective_energy_difference('prot_bound', 'unbound', 'bound', **out_c)
+            sod_dg[j, i], _ = scanner.effective_energy_difference('sod_bound', 'unbound', 'bound', **out_c)
+
+    plot_msp(pH, Na, scanner, microstate_probs_dir)
+    plot_free_energies(pH, Na, scanner, free_energy_dir)
+    plot_dg(pH, Na, conf_dg, conf_dg_dir)
+    plot_dg(pH, Na, prot_dg, protonation_dg_dir)
+    plot_dg(pH, Na, sod_dg, sod_bind_dg_dir)
+
+    breakpoint()
+
+    edges = [('IFH', 'IF0'),
+             ('IF0', 'IFNA'),
+             ('IFNA', 'OFNA'),
+             ('OFNA', 'OF0'),
+             ('OF0', 'OFH'),
+             ('OFH', 'IFH')]
+
+    concentrations['H+'] = [8]
+    scanner.run(concentrations, svd=False)
+
+    dg = scanner.results.free_energy.sel({'pH': 8, 'Na+': 0.100})
+    std_err = scanner.results.std_errors.sel({'pH': 8, 'Na+': 0.100})
+    print(dg)
+
+    values = []
+
+    for i, j in edges:
+        dg_i = dg.sel(state=i).values
+        dg_j = dg.sel(state=j).values
+        si = std_err.sel(state=i).values
+        sj = std_err.sel(state=j).values
+        std_err_ij = np.sqrt(si**2 + sj**2)
+        diff = dg_j - dg_i
+        print(f'{i} ({dg_i}) --> {j} ({dg_j}) => {diff} ± {std_err_ij}')
+        values.append(diff)
+    print(np.sum(values))
+
+
+def plot_dg(pH, Na, data, outdir, ylim=[None, None]):
+    fig, ax = plt.subplots(ncols=1, nrows=1, figsize=(3, 2))
+
+    for i in range(len(Na)):
+        ax.plot(pH, data[i, :], label=f'{Na[i]:0.3f} M', color=sod_colors[i])
+
+    ax.axhline(0, ls='--', color='black', alpha=0.5)
+
+    ax.legend(loc='best')
+    ax.set_xlim([0, 15])
+
+    filename = outdir / 'profile.pdf'
+    plt.savefig(filename)
+    plt.tight_layout()
+    plt.cla()
+
+
+def plot_free_energies(pH, Na, scanner, outdir):
+    states = scanner.results.state.values
+
+    ds = scanner.results
+
+    for c_na in Na:
+        fig, ax = plt.subplots(ncols=1, nrows=1, figsize=(3, 2))
+        dg = ds.free_energy.sel({'Na+': c_na}).values
+
+        for i in range(6):
+            state_name = state_map[states[i]]
+            ax.plot(pH, dg[i, :], label=state_name, color=state_colors[states[i]])
+        ax.legend(loc='best')
+
+        ax.set_xlim([0, 15])
+
+        sns.despine(offset=5, ax=ax)
+
+        filename = outdir / f'na_{c_na}.pdf'
+        plt.tight_layout()
+        plt.savefig(filename)
+        plt.clf()
+
+
+def plot_msp(pH, Na, scanner, outdir):
+    states = scanner.results.state.values
+
+    ds = scanner.results
+
+    for c_na in Na:
+        fig, ax = plt.subplots(ncols=1, nrows=1, figsize=(3, 2))
+        probs = ds.microstate_probs.sel({'Na+': c_na}).values
+
+        for i in range(6):
+            state_name = state_map[states[i]]
+            ax.plot(pH, probs[i, :], label=state_name, color=state_colors[states[i]])
+        ax.legend(loc='best')
+
+        ax.set_xlim([0, 15])
+        ax.set_ylim([0, 1])
+
+        sns.despine(offset=5, ax=ax)
+
+        filename = outdir / f'na_{c_na}.pdf'
+        plt.tight_layout()
+        plt.savefig(filename)
+        plt.clf()
+
+
+if __name__ == "__main__":
+    run()
---
+++ full_antiporter_example/generate_paper_tables.py
@@ -0,0 +1,152 @@
+from multibind.nonequilibrium import rate_matrix
+import numpy as np
+import math
+
+
+ifh = "IFH"
+if0 = "IF0"
+ifn = "IFNA"
+ofh = "OFH"
+of0 = "OF0"
+ofn = "OFNA"
+
+
+ordering = [(ifh, ofh),
+            (ofh, of0),
+            (of0, ofn),
+            (ofn, ifn),
+            (ifn, if0),
+            (if0, ifh),
+            ]
+
+
+def dG2pKa(dG, pH):
+    return pH - dG / math.log(10)
+
+
+def format_name(name):
+    if "NA" in name:
+        return name[0:2] + r"(Na$^+$)"
+    if "H" in name:
+        return name[0:2] + r"(H$^+$)"
+    if "0" in name:
+        return name[0:2] + r"(0)"
+
+
+def table_from_entries(entries, bars=True, dG_err=None):
+    table = """
+\\begin{table}[]
+\\begin{tabular}{@{}lllll@{}}
+\\toprule
+State 1 & State 2 & $\\bar k_{12} \\pm \\bar \\sigma_{12}$ (s$^{-1}$) & $\\bar k_{21} \\pm \\bar \\sigma_{21}$ (s$^{-1}$) & $\\Delta \\bar G_{12} \\pm \\bar \\sigma_{\\Delta G}$ ($kT$) \\\\ \\midrule"""
+
+    ordered_entries = []
+    for i, (s1, s2) in enumerate(ordering):
+        entry = None
+        for j, e in enumerate(entries):
+            if s1 == e[0] and s2 == e[1]:
+                entry = e
+                break
+        if not entry:
+            raise ValueError
+        ordered_entries.append(entry)
+
+    if dG_err:
+        assert len(ordered_entries) == len(dG_err)
+
+    for i, entry in enumerate(ordered_entries):
+        s1, s2, k, var = entry
+        bs1, bs2, bk, bvar = list(filter(lambda x: x[1] == s1 and x[0] == s2, entries))[0]
+
+        dG = f"{-math.log(k / bk):0.3f}"
+        dG_std = math.sqrt(var / k**2 + bvar / bk**2)
+
+        if dG_err:
+            dG_std = dG_err[i]
+
+        append_value = f"{format_name(s1)} & {format_name(s2)} & ${k:0.2f} \\pm {var**0.5:0.2f}$ & ${bk:0.2f} \\pm {bvar**0.5:0.2f}$  & ${dG} \\pm {dG_std:0.3f}$ \\\\"
+
+        table += f"\n{append_value}"
+
+    table += r""" \bottomrule
+\end{tabular}
+\end{table}
+"""
+
+    if not bars:
+        table = table.replace("\\bar", '')
+
+    print(table)
+
+
+def raw_rates_table(rate_file):
+
+    entries = []
+    with open(rate_file, 'r') as F:
+        for _line in F:
+            line = _line.strip().split(",")
+            if line[0] == 'state1':
+                continue
+            s1, s2, v, sigma = line
+            entries.append((s1, s2, float(v), float(sigma)))
+
+    table_from_entries(entries, bars=True)
+
+
+def corrected_rates_table(rate_file):
+    pH = 8
+    Na = 0.1  # 100 mM
+
+    c, k, kstd = rate_matrix(rate_file)
+
+    states = c.states.values
+    #  free_energies = c.g_mle
+    g_std_err = c.std_errors
+
+    new_graph = c.graph.copy()
+
+    for index, data in new_graph.iterrows():
+        state1, state2, value, variance, ligand, std = data
+        # print(state1, state2, value, variance, ligand, std)
+
+        if (state1[-1] == "H" and state2[-1] == "0") or (state1[-1] == "A" and state2[-1] == "0"):
+            # backwards proton reaction
+            new_graph.at[index, 'state1'] = state2
+            new_graph.at[index, 'state2'] = state1
+            new_graph.at[index, 'value'] = -value
+
+            value = new_graph.value[index]
+            state1 = new_graph.state1[index]
+            state2 = new_graph.state2[index]
+
+        if state1[-1] == "0" and state2[-1] == "H":
+            new_graph.at[index, 'ligand'] = "H+"
+            new_graph.at[index, 'value'] = dG2pKa(new_graph.value[index], pH)
+            new_graph.at[index, 'variance'] = new_graph.variance[index] / np.log(10)**2
+
+        if state1[-1] == "0" and state2[-1] == "A":
+            new_graph.at[index, 'ligand'] = "Na+"
+            new_graph.at[index, 'value'] = new_graph.value[index] + np.log(Na)
+
+    dG_err = []
+    entries = []
+    for s1, s2 in ordering:
+        s1_idx = list(filter(lambda x: x[1][0] == s1, enumerate(states)))[0][0]
+        s2_idx = list(filter(lambda x: x[1][0] == s2, enumerate(states)))[0][0]
+
+        entries.append((s1, s2, k[s1_idx, s2_idx], kstd[s1_idx, s2_idx]))
+        entries.append((s2, s1, k[s2_idx, s1_idx], kstd[s2_idx, s1_idx]))
+        dG_err.append(math.sqrt(g_std_err[s2_idx]**2 + g_std_err[s1_idx]**2))
+
+    table_from_entries(entries, bars=False, dG_err=dG_err)
+
+
+def main():
+    rate_file = "inputs/rates.csv"
+
+    raw_rates_table(rate_file)
+    corrected_rates_table(rate_file)
+
+
+if __name__ == "__main__":
+    main()
